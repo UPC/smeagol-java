55a44dd80e5b23c5acd092367eeb59c1008764cb
Angel Aguilera <angel.aguilera@gmail.com>
2010-12-09 13:18:19 +0000

    Esquelet de tests per a reserves recurrents
    git-svn-id: http://devel.cpl.upc.edu/recursos/svn/smeagol/java/branches/t251@1308 4ab63a03-c2d3-4037-8559-9a21b594da05

---
+++ smeagol-client/src/test/java/edu/upc/cpl/smeagol/client/domain/RecurringBookingTest.java
@@ -0,0 +1,44 @@
+package edu.upc.cpl.smeagol.client.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class RecurringBookingTest {
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testSerialize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSerializeCollectionOfRecurringBooking() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeserialize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeserializeCollection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCompareTo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+}
